7236bcbcfba2d11898a0ba70ba04d749f1cf7aaa
Your Name~ <you@example.com>
2022-02-22 13:33:52 +0200

    goodbye git and 1 more sentence

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Git!");
         System.out.println("Hello, Git!");
-        System.out.println("Hello, Git!");
+        System.out.println("Goodbye, Git!");
+        System.out.println("Goodbye, Git!");
     }
 }
